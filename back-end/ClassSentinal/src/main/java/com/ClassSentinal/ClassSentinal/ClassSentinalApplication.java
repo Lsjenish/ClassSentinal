fa3eb0c81df8cde954835b6c5e191cca56ebd4f0
Lsjenish <jeniston2005@gmail.com>
2025-11-02 17:57:37 +0530

    After set up project setup

---
+++ back-end/ClassSentinal/src/main/java/com/ClassSentinal/ClassSentinal/ClassSentinalApplication.java
@@ -0,0 +1,13 @@
+package com.ClassSentinal.ClassSentinal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSentinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassSentinalApplication.class, args);
+	}
+
+}
---
+++ back-end/ClassSentinal/src/test/java/com/ClassSentinal/ClassSentinal/ClassSentinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ClassSentinal.ClassSentinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassSentinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
