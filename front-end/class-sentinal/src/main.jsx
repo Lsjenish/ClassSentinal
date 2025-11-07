3bf906024363bbd82d5cf55e3f068a154121333a
Lsjenish <jeniston2005@gmail.com>
2025-11-07 21:01:26 +0530

    Prepare frontend for gh-pages deployment

--- front-end/class-sentinal/src/main.jsx
+++ front-end/class-sentinal/src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { BrowserRouter } from 'react-router-dom'
+import {HashRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import { store } from './state/store.js'
 
@@ -10,11 +10,11 @@ createRoot(document.getElementById('root')).render(
   
 
   <Provider store={store}>
-    <BrowserRouter>
+    <HashRouter >
   <StrictMode>
     <App />
   </StrictMode>,
-  </BrowserRouter>  
+  </HashRouter >  
   </Provider>
 
 )
